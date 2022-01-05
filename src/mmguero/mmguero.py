c65dbbc841039207c9c82b0c3b3abbc45d8d7f03
SG <13872653+mmguero@users.noreply.github.com>
2022-01-05 13:33:55 -0700

    added deep_get

--- src/mmguero/mmguero.py
+++ src/mmguero/mmguero.py
@@ -58,6 +58,23 @@ def eprint(*args, **kwargs):
 
 
 ###################################################################################################
+# safe deep get for a dictionary
+#
+# Example:
+#   d = {'meta': {'status': 'OK', 'status_code': 200}}
+#   deep_get(d, ['meta', 'status_code'])          # => 200
+#   deep_get(d, ['garbage', 'status_code'])       # => None
+#   deep_get(d, ['meta', 'garbage'], default='-') # => '-'
+def deep_get(d, keys, default=None):
+    assert type(keys) is list
+    if d is None:
+        return default
+    if not keys:
+        return d
+    return deep_get(d.get(keys[0]), keys[1:], default)
+
+
+###################################################################################################
 # get interactive user response to Y/N question
 def YesOrNo(question, default=None, forceInteraction=False, acceptDefault=False):
 
